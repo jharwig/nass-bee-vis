7d4253129378737e45ca53b8362995e7cc8627fc
Rupayan Neogy <rupayan.neogy@kensho.com>
2020-07-09 15:31:17 -0400

    testing mapchart2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 
-import MapChart from './MapChart'
+import MapChart2 from './MapChart2'
 
 export default function App(): JSX.Element {
   return (
     <span aria-label="skull" role="img">
       🐝
-      <MapChart />
+      <MapChart2 />
     </span>
   )
 }
