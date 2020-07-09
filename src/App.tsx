3d81f49849d14f53fefa4b493dc8087c151f10b4
Filip Osowski <filip.osowski@kensho.com>
2020-07-09 12:17:01 -0500

    Add basic map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import MapChart from './MapChart'
 
 export default function App(): JSX.Element {
   return (
     <span aria-label="skull" role="img">
       🐝
+      <MapChart />
     </span>
   )
 }
